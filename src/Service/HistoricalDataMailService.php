a05acaa840d07775b3d52bc22574e8a82933b3cf
gp <gabe777@gmail.com>
2025-01-25 07:42:51 +0100

    - enviroment variables sanitized  - MailService simplified

--- src/Service/HistoricalDataMailService.php
+++ src/Service/HistoricalDataMailService.php
@@ -11,32 +11,19 @@ class HistoricalDataMailService
 {
     private const string FROM_MAIL = 'no-reply@example.com';
 
-    private ?Email $mail = null;
-
     public function __construct(private readonly MailerInterface $mailer)
     {
     }
 
-    public function getEmail(): Email
+    public function send(string $recipient, string $subject, string $body, string $csvContent): void
     {
-        if (null === $this->mail) {
-            $this->mail = new Email();
-        }
-
-        return $this->mail;
-    }
-
-    public function send(string $recipient, string $subject, string $body, string $csvContent): Email
-    {
-        $this->getEmail()->from(self::FROM_MAIL)->to($recipient)->subject($subject)->text($body)->attach(
+        $email = (new Email())->from(self::FROM_MAIL)->to($recipient)->subject($subject)->text($body)->attach(
             $csvContent,
             'historical_data.csv',
             'text/csv'
         );
 
-        $this->mailer->send($this->getEmail());
-
-        return $this->getEmail();
+        $this->mailer->send($email);
     }
 
 }
